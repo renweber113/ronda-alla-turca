2aa0f50dada4b4b6ad95bb20d2c81a5682eef9e4
renweber113 <renweber113@gmail.com>
2013-11-03 18:21:54 -0800

    Adding other files

--- pizzaroni-1/helloworld.py
+++ pizzaroni-1/helloworld.py
@@ -1,3 +1,38 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello Ren Weber'
+#http://www.youtube.com/watch?v=U1KhwqVa9fo
+  
+import webapp2
+import cgi
+import os
+import jinja2
+from gaesessions import get_current_session
+ 
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        session = get_current_session()
+        firstName = session.get('firstName', '')
+        familyName = session.get('familyName', '')
+        message = session.get('message', '')
+        jinja_environment = jinja2.Environment(autoescape = True,
+            loader = jinja2.FileSystemLoader(os.path.join(
+               os.path.dirname(__file__), 'templates')))
+        tpl_vars = { "message": message, "firstName": firstName,
+            "familyName": familyName }
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(tpl_vars))
+    
+    def post(self):
+        firstName = self.request.get("firstName")
+        familyName = self.request.get("familyName")
+        session = get_current_session()
+        session['firstName'] = firstName
+        session['familyName'] = familyName
+        session['message'] = ''
+        if len(firstName) < 2 or len(familyName) < 2:
+            session['message'] = "First Name and Family Name are mandatory"
+            self.redirect("/")
+        self.response.out.write("First Name: " + firstName
+           + " Family Name: " + familyName)
+ 
+app = webapp2.WSGIApplication([('/', MainHandler)],
+                              debug=True)
+
